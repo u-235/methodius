52219ab4f35d0cef6161c05b01207430d2c21d46
u-235 <nicegorov@yandex.ru>
2014-01-28 17:35:43 +0400

    documented PixselMapListener

--- src/microfont/events/PixselMapListener.java
+++ src/microfont/events/PixselMapListener.java
@@ -3,6 +3,14 @@ package microfont.events;
 
 import java.util.EventListener;
 
+/**
+ * Интерфейс получателя сообщений от карты пикселей.
+ */
 public interface PixselMapListener extends EventListener {
+    /**
+     * Метод получателя сообщений при изменении пикселей.
+     * 
+     * @param event Сообщение об изменении пикселей.
+     */
     public void pixselChanged(PixselMapEvent event);
 }
