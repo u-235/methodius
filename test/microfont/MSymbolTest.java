f3554f0a3678f85fb3c5f70c394358ddb93134c8
u-235 <nicegorov@yandex.ru>
2014-11-26 22:22:28 +0300

    create MSymbolTest.java

---
+++ test/microfont/MSymbolTest.java
@@ -0,0 +1,167 @@
+package microfont;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class MSymbolTest extends PixselMapTest {
+
+    @Test
+    public void testGetCode() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetCode() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testIsUnicode() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetUnicode() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testClearUnicode() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetUnicode() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetOwner() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCopyMSymbol() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCopyAbstractPixselMap() {
+        fail("Not yet implemented");
+    }
+
+    @Override
+    @Test
+    public void testSetSizeIntInt() {
+        fail("Not yet implemented");
+    }
+
+    @Override
+    @Test
+    public void testSetSizeDimension() {
+        fail("Not yet implemented");
+    }
+
+    @Override
+    @Test
+    public void testSetWidth() {
+        fail("Not yet implemented");
+    }
+
+    @Override
+    @Test
+    public void testSetHeight() {
+        fail("Not yet implemented");
+    }
+
+    @Override
+    @Test
+    public void testChangeWidth() {
+        fail("Not yet implemented");
+    }
+
+    @Override
+    @Test
+    public void testChangeHeight() {
+        fail("Not yet implemented");
+    }
+
+    @Override
+    @Test
+    public void testRemoveColumns() {
+        fail("Not yet implemented");
+    }
+
+    @Override
+    @Test
+    public void testRemoveRows() {
+        fail("Not yet implemented");
+    }
+
+    @Override
+    @Test
+    public void testRemoveLeft() {
+        fail("Not yet implemented");
+    }
+
+    @Override
+    @Test
+    public void testRemoveRight() {
+        fail("Not yet implemented");
+    }
+
+    @Override
+    @Test
+    public void testRemoveTop() {
+        fail("Not yet implemented");
+    }
+
+    @Override
+    @Test
+    public void testRemoveBottom() {
+        fail("Not yet implemented");
+    }
+
+    @Override
+    @Test
+    public void testAddColumns() {
+        fail("Not yet implemented");
+    }
+
+    @Override
+    @Test
+    public void testAddRows() {
+        fail("Not yet implemented");
+    }
+
+    @Override
+    @Test
+    public void testAddLeft() {
+        fail("Not yet implemented");
+    }
+
+    @Override
+    @Test
+    public void testAddRight() {
+        fail("Not yet implemented");
+    }
+
+    @Override
+    @Test
+    public void testAddTop() {
+        fail("Not yet implemented");
+    }
+
+    @Override
+    @Test
+    public void testAddBottom() {
+        fail("Not yet implemented");
+    }
+
+    @Override
+    @Test
+    public void testRotate() {
+        fail("Not yet implemented");
+    }
+
+}
