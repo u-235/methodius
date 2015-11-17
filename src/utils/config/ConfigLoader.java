9b2c69c840836944de7284d61eb9f71c13b241fc
u-235 <nicegorov@yandex.ru>
2015-11-18 02:57:57 +0300

    Задокументирован

--- src/utils/config/ConfigLoader.java
+++ src/utils/config/ConfigLoader.java
@@ -4,6 +4,15 @@ package utils.config;
 import java.io.Closeable;
 import java.io.IOException;
 
+/**
+ * Интерфейс загрузчика конфигурации из потока.
+ */
 public interface ConfigLoader extends Closeable {
-    public void load() throws IOException,  InterruptedException;
+    /**
+     * Загрузка конфигурации из потока.
+     * 
+     * @throws IOException
+     * @throws InterruptedException Если во время загрузки задача была прервана.
+     */
+    public void load() throws IOException, InterruptedException;
 }
--- src/utils/config/ConfigSaver.java
+++ src/utils/config/ConfigSaver.java
@@ -4,6 +4,16 @@ package utils.config;
 import java.io.Closeable;
 import java.io.IOException;
 
+/**
+ * Интерфейс для сохранения настроек в поток.
+ */
 public interface ConfigSaver extends Closeable {
-    public void save() throws IOException,  InterruptedException;
+    /**
+     * Сохранение конфигурации в поток.
+     * 
+     * @throws IOException
+     * @throws InterruptedException Если во время сохранения задача была
+     *             прервана.
+     */
+    public void save() throws IOException, InterruptedException;
 }
